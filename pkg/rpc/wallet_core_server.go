fb540fd1de1e454910ec5c1ac9a16ffde218834f
wenweih <mr.huangwenwei@gmail.com>
2018-12-11 13:09:28 +0800

    Rename wallet_core.go file

--- pkg/rpc/wallet_core_server.go
+++ pkg/rpc/wallet_core_server.go
@@ -5,6 +5,7 @@ import (
   "errors"
   "context"
   "wallet-transition/pkg/pb"
+  "github.com/btcsuite/btcutil"
   "github.com/btcsuite/btcutil/hdkeychain"
   "github.com/btcsuite/btcd/chaincfg"
 )
@@ -38,4 +39,21 @@ func (s *WalletCoreServerRPC) Address(ctx context.Context, in *proto.AddressReq)
 
   }
   return &proto.AddressResp{Address: address}, nil
+}
+
+func genBTCAddress() (*btcutil.AddressPubKeyHash, error) {
+  seed, err := hdkeychain.GenerateSeed(hdkeychain.RecommendedSeedLen)
+  if err != nil {
+    return nil, errors.New(strings.Join([]string{"GenerateSeed err", err.Error()}, ":"))
+  }
+
+  key, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+  if err != nil {
+    return nil, errors.New(strings.Join([]string{"NewMaster err", err.Error()}, ":"))
+  }
+  add, err := key.Address(&chaincfg.MainNetParams)
+  if err != nil {
+    return nil, errors.New(strings.Join([]string{"NewAddressPubKeyHash err", err.Error()}, ":"))
+  }
+  return add, nil
 }
