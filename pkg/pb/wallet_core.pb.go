7cf4c6dc6232d583cb4a0db04d8e5fa7e79e36c9
wenweih <mr.huangwenwei@gmail.com>
2018-12-14 18:08:57 +0800

    Add SignTx rpc method

--- pkg/pb/wallet_core.pb.go
+++ pkg/pb/wallet_core.pb.go
@@ -100,24 +100,134 @@ func (m *AddressResp) GetAddress() string {
 	return ""
 }
 
+type SignTxReq struct {
+	From                 string   `protobuf:"bytes,1,opt,name=from,proto3" json:"from,omitempty"`
+	To                   string   `protobuf:"bytes,2,opt,name=to,proto3" json:"to,omitempty"`
+	Amount               float64  `protobuf:"fixed64,3,opt,name=amount,proto3" json:"amount,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *SignTxReq) Reset()         { *m = SignTxReq{} }
+func (m *SignTxReq) String() string { return proto.CompactTextString(m) }
+func (*SignTxReq) ProtoMessage()    {}
+func (*SignTxReq) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5e25c9835eecce9f, []int{2}
+}
+
+func (m *SignTxReq) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignTxReq.Unmarshal(m, b)
+}
+func (m *SignTxReq) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignTxReq.Marshal(b, m, deterministic)
+}
+func (m *SignTxReq) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignTxReq.Merge(m, src)
+}
+func (m *SignTxReq) XXX_Size() int {
+	return xxx_messageInfo_SignTxReq.Size(m)
+}
+func (m *SignTxReq) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignTxReq.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignTxReq proto.InternalMessageInfo
+
+func (m *SignTxReq) GetFrom() string {
+	if m != nil {
+		return m.From
+	}
+	return ""
+}
+
+func (m *SignTxReq) GetTo() string {
+	if m != nil {
+		return m.To
+	}
+	return ""
+}
+
+func (m *SignTxReq) GetAmount() float64 {
+	if m != nil {
+		return m.Amount
+	}
+	return 0
+}
+
+type SignTxResp struct {
+	Result               bool     `protobuf:"varint,1,opt,name=result,proto3" json:"result,omitempty"`
+	RawTx                string   `protobuf:"bytes,2,opt,name=rawTx,proto3" json:"rawTx,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *SignTxResp) Reset()         { *m = SignTxResp{} }
+func (m *SignTxResp) String() string { return proto.CompactTextString(m) }
+func (*SignTxResp) ProtoMessage()    {}
+func (*SignTxResp) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5e25c9835eecce9f, []int{3}
+}
+
+func (m *SignTxResp) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignTxResp.Unmarshal(m, b)
+}
+func (m *SignTxResp) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignTxResp.Marshal(b, m, deterministic)
+}
+func (m *SignTxResp) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignTxResp.Merge(m, src)
+}
+func (m *SignTxResp) XXX_Size() int {
+	return xxx_messageInfo_SignTxResp.Size(m)
+}
+func (m *SignTxResp) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignTxResp.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignTxResp proto.InternalMessageInfo
+
+func (m *SignTxResp) GetResult() bool {
+	if m != nil {
+		return m.Result
+	}
+	return false
+}
+
+func (m *SignTxResp) GetRawTx() string {
+	if m != nil {
+		return m.RawTx
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*AddressReq)(nil), "proto.addressReq")
 	proto.RegisterType((*AddressResp)(nil), "proto.AddressResp")
+	proto.RegisterType((*SignTxReq)(nil), "proto.SignTxReq")
+	proto.RegisterType((*SignTxResp)(nil), "proto.SignTxResp")
 }
 
 func init() { proto.RegisterFile("wallet_core.proto", fileDescriptor_5e25c9835eecce9f) }
 
 var fileDescriptor_5e25c9835eecce9f = []byte{
-	// 133 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2c, 0x4f, 0xcc, 0xc9,
-	0x49, 0x2d, 0x89, 0x4f, 0xce, 0x2f, 0x4a, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05,
-	0x53, 0x4a, 0x4a, 0x5c, 0x5c, 0x89, 0x29, 0x29, 0x45, 0xa9, 0xc5, 0xc5, 0x41, 0xa9, 0x85, 0x42,
-	0x22, 0x5c, 0xac, 0x89, 0xc5, 0xc5, 0xa9, 0x25, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x10,
-	0x8e, 0x92, 0x3a, 0x17, 0xb7, 0x23, 0x4c, 0x4d, 0x71, 0x81, 0x90, 0x04, 0x17, 0x3b, 0x54, 0x0b,
-	0x54, 0x19, 0x8c, 0x6b, 0x64, 0xc7, 0xc5, 0x15, 0x0e, 0xb6, 0xc8, 0x39, 0xbf, 0x28, 0x55, 0xc8,
-	0x80, 0x8b, 0x1d, 0xaa, 0x4d, 0x48, 0x10, 0x62, 0xa9, 0x1e, 0xc2, 0x2a, 0x29, 0x21, 0xa8, 0x10,
-	0x92, 0xc9, 0x49, 0x6c, 0x60, 0x21, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0xee, 0x09, 0x91,
-	0xc8, 0xaf, 0x00, 0x00, 0x00,
+	// 231 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x8f, 0x41, 0x4b, 0xc3, 0x40,
+	0x14, 0x84, 0x49, 0xb4, 0xa9, 0x1d, 0x41, 0xcc, 0x43, 0x24, 0xf4, 0x54, 0xf6, 0x62, 0x2f, 0x16,
+	0xd1, 0x9b, 0x37, 0xf1, 0xe0, 0x3d, 0x16, 0x3c, 0xca, 0x6a, 0x57, 0x11, 0x92, 0xbe, 0xb8, 0x6f,
+	0x4b, 0xf3, 0xf3, 0x25, 0xbb, 0x2f, 0x2a, 0x3d, 0x25, 0x33, 0x3b, 0x3b, 0x3b, 0x1f, 0xca, 0xbd,
+	0x6d, 0x1a, 0x17, 0x5e, 0xdf, 0xd9, 0xbb, 0x55, 0xe7, 0x39, 0x30, 0x4d, 0xe2, 0xc7, 0x18, 0xc0,
+	0x6e, 0x36, 0xde, 0x89, 0xd4, 0xee, 0x9b, 0x2e, 0x30, 0xb1, 0x22, 0x2e, 0x54, 0xd9, 0x22, 0x5b,
+	0xce, 0xea, 0x24, 0xcc, 0x15, 0x4e, 0x1f, 0xc6, 0x8c, 0x74, 0x54, 0x61, 0xaa, 0x57, 0x34, 0x36,
+	0x4a, 0xf3, 0x84, 0xd9, 0xf3, 0xd7, 0xe7, 0x76, 0xdd, 0x0f, 0x5d, 0x84, 0xe3, 0x0f, 0xcf, 0xad,
+	0x66, 0xe2, 0x3f, 0x9d, 0x21, 0x0f, 0x5c, 0xe5, 0xd1, 0xc9, 0x03, 0xd3, 0x25, 0x0a, 0xdb, 0xf2,
+	0x6e, 0x1b, 0xaa, 0xa3, 0x45, 0xb6, 0xcc, 0x6a, 0x55, 0xe6, 0x1e, 0x18, 0x8b, 0xa4, 0x1b, 0x52,
+	0xde, 0xc9, 0xae, 0x49, 0xb3, 0x4e, 0x6a, 0x55, 0xc3, 0x5a, 0x6f, 0xf7, 0xeb, 0x5e, 0x0b, 0x93,
+	0xb8, 0x6d, 0x81, 0x97, 0x48, 0xfb, 0xc8, 0xde, 0xd1, 0x0d, 0xa6, 0xba, 0x9d, 0xca, 0x44, 0xbe,
+	0xfa, 0xe3, 0x9d, 0x93, 0x5a, 0xff, 0xf1, 0xae, 0x51, 0xa4, 0xb7, 0xe9, 0x5c, 0x4f, 0x7f, 0x99,
+	0xe6, 0xe5, 0x81, 0x23, 0xdd, 0x5b, 0x11, 0x9d, 0xbb, 0x9f, 0x00, 0x00, 0x00, 0xff, 0xff, 0xc0,
+	0x16, 0xd6, 0x6f, 0x63, 0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -133,6 +243,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type WalletCoreClient interface {
 	Address(ctx context.Context, in *AddressReq, opts ...grpc.CallOption) (*AddressResp, error)
+	SignTx(ctx context.Context, in *SignTxReq, opts ...grpc.CallOption) (*SignTxResp, error)
 }
 
 type walletCoreClient struct {
@@ -152,9 +263,19 @@ func (c *walletCoreClient) Address(ctx context.Context, in *AddressReq, opts ...
 	return out, nil
 }
 
+func (c *walletCoreClient) SignTx(ctx context.Context, in *SignTxReq, opts ...grpc.CallOption) (*SignTxResp, error) {
+	out := new(SignTxResp)
+	err := c.cc.Invoke(ctx, "/proto.WalletCore/SignTx", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // WalletCoreServer is the server API for WalletCore service.
 type WalletCoreServer interface {
 	Address(context.Context, *AddressReq) (*AddressResp, error)
+	SignTx(context.Context, *SignTxReq) (*SignTxResp, error)
 }
 
 func RegisterWalletCoreServer(s *grpc.Server, srv WalletCoreServer) {
@@ -179,6 +300,24 @@ func _WalletCore_Address_Handler(srv interface{}, ctx context.Context, dec func(
 	return interceptor(ctx, in, info, handler)
 }
 
+func _WalletCore_SignTx_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SignTxReq)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(WalletCoreServer).SignTx(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/proto.WalletCore/SignTx",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(WalletCoreServer).SignTx(ctx, req.(*SignTxReq))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _WalletCore_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "proto.WalletCore",
 	HandlerType: (*WalletCoreServer)(nil),
@@ -186,6 +325,10 @@ var _WalletCore_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Address",
 			Handler:    _WalletCore_Address_Handler,
+		},
+		{
+			MethodName: "SignTx",
+			Handler:    _WalletCore_SignTx_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
