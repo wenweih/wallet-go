35d9705793ff64242173aabd350bd939c8cc4d1d
wenweih <mr.huangwenwei@gmail.com>
2019-01-09 09:49:58 +0800

    Add SendToAddressSignBTC rpc method

--- pkg/rpc/wallet_core_server.go
+++ pkg/rpc/wallet_core_server.go
@@ -41,6 +41,72 @@ func (s *WalletCoreServerRPC) Address(ctx context.Context, in *proto.AddressReq)
   return &proto.AddressResp{Address: address}, nil
 }
 
+// SendToAddressSignBTC btc sendtoaddress tx signature
+func (s *WalletCoreServerRPC) SendToAddressSignBTC(ctx context.Context, in *proto.SendToAddressReq) (*proto.SignTxResp, error) {
+  ldb, err := db.NewLDB("btc")
+  if err != nil {
+    return nil, err
+  }
+  defer ldb.Close()
+
+  tx, err := blockchain.DecodeBtcTxHex(in.HexUnsignedTx)
+  if err != nil {
+    return nil, errors.New(strings.Join([]string{"fail to DecodeBtcTxHex", err.Error()}, ":"))
+  }
+  var subscriptNewEngine []byte
+  for _, txIn := range tx.MsgTx().TxIn {
+    pOutPoint := txIn.PreviousOutPoint
+    for i, utxo := range in.Utxo {
+      if pOutPoint.Hash.String() == utxo.Txid && pOutPoint.Index == utxo.Index {
+        address, err := btcutil.DecodeAddress(utxo.Address, &chaincfg.RegressionNetParams)
+        if err != nil {
+          return nil, errors.New(strings.Join([]string{"DecodeAddress error", err.Error()}, ":"))
+        }
+        subscript, err := txscript.PayToAddrScript(address)
+        if err != nil {
+          return nil, errors.New(strings.Join([]string{"PayToAddrScript error", err.Error()}, ":"))
+        }
+
+        if i == 0 {
+          subscriptNewEngine = subscript
+        }
+
+        priv, err := ldb.Get([]byte(utxo.Address), nil)
+        if err != nil && strings.Contains(err.Error(), "leveldb: not found") {
+          return nil, errors.New(strings.Join([]string{"Address:", utxo.Address, " not found: ", err.Error()}, ""))
+        }
+
+        wif, err := btcutil.DecodeWIF(string(priv[:]))
+        if err != nil {
+          return nil, errors.New(strings.Join([]string{"fail to decode wif", err.Error()}, ":"))
+        }
+
+        sigScript, err := txscript.SignatureScript(tx.MsgTx(), i, subscript, txscript.SigHashAll, wif.PrivKey, true)
+        if err != nil {
+          return nil, errors.New(strings.Join([]string{"SignatureScript error", err.Error()}, ":"))
+        }
+        txIn.SignatureScript = sigScript
+      }
+    }
+  }
+
+  //Validate signature
+  flags := txscript.StandardVerifyFlags
+  vm, err := txscript.NewEngine(subscriptNewEngine, tx.MsgTx(), 0, flags, nil, nil, in.VinAmount)
+  if err != nil {
+    return nil, errors.New(strings.Join([]string{"txscript.NewEngine: ", err.Error()}, ":"))
+  }
+  if err := vm.Execute(); err != nil {
+    return nil, errors.New(strings.Join([]string{"fail to sign tx ", err.Error()}, ":"))
+  }
+
+  // txToHex
+  buf := bytes.NewBuffer(make([]byte, 0, tx.MsgTx().SerializeSize()))
+  tx.MsgTx().Serialize(buf)
+  txHex := hex.EncodeToString(buf.Bytes())
+  return &proto.SignTxResp{Result: true, HexSignedTx: txHex}, nil
+}
+
 // SignTx sign raw tx
 func (s *WalletCoreServerRPC) SignTx(ctx context.Context, in *proto.SignTxReq) (*proto.SignTxResp, error) {
   from := in.From
