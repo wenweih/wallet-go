c6197596d0dec2ac77075af9aa9629867c8ba71e
wenweih <mr.huangwenwei@gmail.com>
2019-03-04 14:14:31 +0800

    Add coin_select.go

--- pkg/blockchain/bitcoin.go
+++ pkg/blockchain/bitcoin.go
@@ -379,56 +379,6 @@ func DecodeBtcTxHex(txHex string) (*btcutil.Tx, error) {
 	return btcutil.NewTx(&msgTx), nil
 }
 
-// Hash implements coinset Coin interface
-func (c *SimpleCoin) Hash() *chainhash.Hash { return c.TxHash }
-// Index implements coinset Coin interface
-func (c *SimpleCoin) Index() uint32         { return c.TxIndex }
-// Value implements coinset Coin interface
-func (c *SimpleCoin) Value() btcutil.Amount { return c.TxValue }
-// PkScript implements coinset Coin interface
-func (c *SimpleCoin) PkScript() []byte      { return nil }
-// NumConfs implements coinset Coin interface
-func (c *SimpleCoin) NumConfs() int64       { return c.TxNumConfs }
-// ValueAge implements coinset Coin interface
-func (c *SimpleCoin) ValueAge() int64       { return int64(c.TxValue) * c.TxNumConfs }
-
-// CoinSelect btc tx inputs
-func CoinSelect(bheader int64, txAmount btcutil.Amount, utxos []db.UTXO) ([]db.UTXO, coinset.Coins, error) {
-	var coins []coinset.Coin
-	for _, utxo := range utxos {
-		txHash, err := chainhash.NewHashFromStr(utxo.Txid)
-		if err != nil {
-			return nil, nil, errors.New(strings.Join([]string{"convert utxo hexTxid to txHash error: ", err.Error()}, ""))
-		}
-		amount, err := btcutil.NewAmount(utxo.Amount)
-		if err != nil {
-			return nil, nil, errors.New(strings.Join([]string{"convert utxo amount(float64) to btc amount(int64 as Satoshi) error: ", err.Error()}, ""))
-		}
-		coins = append(coins, coinset.Coin(&SimpleCoin{TxHash: txHash, TxIndex: utxo.VoutIndex, TxValue: amount, TxNumConfs: bheader - utxo.Height + 1}))
-	}
-
-	selector := &coinset.MaxValueAgeCoinSelector{
-		MaxInputs: 50,
-		MinChangeAmount: 10000,
-	}
-
-	selectedCoins, err := selector.CoinSelect(txAmount, coins)
-	if err != nil {
-		return nil, nil, errors.New(strings.Join([]string{"CoinSelect error: ", err.Error()}, ""))
-	}
-	scoins := selectedCoins.Coins()
-
-	var selectedUTXOs []db.UTXO
-	for _, coin := range scoins {
-		for _, utxo := range utxos {
-			if coin.Hash().String() == utxo.Txid && coin.Index() == utxo.VoutIndex {
-				selectedUTXOs = append(selectedUTXOs, utxo)
-			}
-		}
-	}
-	return selectedUTXOs, selectedCoins, nil
-}
-
 // RawBTCTx btc raw tx
 func RawBTCTx(funbackPkScript, toPkScript []byte, feeKB *btcjson.EstimateFeeResult, btcAmount, txAmount btcutil.Amount, selectedCoins coinset.Coins, isUSDT bool) (*int64, *string, error ) {
 	msgTx := coinset.NewMsgTxWithInputCoins(wire.TxVersion, selectedCoins)
---
+++ pkg/blockchain/coin_select.go
@@ -0,0 +1,59 @@
+package blockchain
+
+import (
+  "fmt"
+  "wallet-transition/pkg/db"
+  "github.com/btcsuite/btcutil"
+  "github.com/btcsuite/btcutil/coinset"
+  "github.com/btcsuite/btcd/chaincfg/chainhash"
+)
+
+// Hash implements coinset Coin interface
+func (c *SimpleCoin) Hash() *chainhash.Hash { return c.TxHash }
+// Index implements coinset Coin interface
+func (c *SimpleCoin) Index() uint32         { return c.TxIndex }
+// Value implements coinset Coin interface
+func (c *SimpleCoin) Value() btcutil.Amount { return c.TxValue }
+// PkScript implements coinset Coin interface
+func (c *SimpleCoin) PkScript() []byte      { return nil }
+// NumConfs implements coinset Coin interface
+func (c *SimpleCoin) NumConfs() int64       { return c.TxNumConfs }
+// ValueAge implements coinset Coin interface
+func (c *SimpleCoin) ValueAge() int64       { return int64(c.TxValue) * c.TxNumConfs }
+
+// CoinSelect btc tx inputs
+func CoinSelect(chainHeader int64, txAmount btcutil.Amount, utxos []db.UTXO) ([]db.UTXO, coinset.Coins, error) {
+	var coins []coinset.Coin
+	for _, utxo := range utxos {
+		txHash, err := chainhash.NewHashFromStr(utxo.Txid)
+		if err != nil {
+      return nil, nil, fmt.Errorf("Convert utxo hexTxid to txHash %s", err)
+		}
+		amount, err := btcutil.NewAmount(utxo.Amount)
+		if err != nil {
+      return nil, nil, fmt.Errorf("Convert utxo amount(float64) to btc amount(int64 as Satoshi) %s", err)
+		}
+		coins = append(coins, coinset.Coin(&SimpleCoin{TxHash: txHash, TxIndex: utxo.VoutIndex, TxValue: amount, TxNumConfs: chainHeader - utxo.Height + 1}))
+	}
+
+	selector := &coinset.MaxValueAgeCoinSelector{
+		MaxInputs: 50,
+		MinChangeAmount: 10000,
+	}
+
+	selectedCoins, err := selector.CoinSelect(txAmount, coins)
+	if err != nil {
+    return nil, nil, fmt.Errorf("CoinSelect error: %s", err)
+	}
+	scoins := selectedCoins.Coins()
+
+	var selectedUTXOs []db.UTXO
+	for _, coin := range scoins {
+		for _, utxo := range utxos {
+			if coin.Hash().String() == utxo.Txid && coin.Index() == utxo.VoutIndex {
+				selectedUTXOs = append(selectedUTXOs, utxo)
+			}
+		}
+	}
+	return selectedUTXOs, selectedCoins, nil
+}
