15576f0905e628922a1a6417555ca347e52c7055
wenweih <mr.huangwenwei@gmail.com>
2019-03-06 12:00:14 +0800

    Add SignatureBitcoincore grpc method

--- pkg/rpc/signature.go
+++ pkg/rpc/signature.go
@@ -2,12 +2,15 @@ package rpc
 
 import (
   "fmt"
+  "bytes"
   "strings"
   "context"
   "encoding/hex"
   "wallet-transition/pkg/pb"
   "wallet-transition/pkg/db"
+  "github.com/btcsuite/btcutil"
   "wallet-transition/pkg/blockchain"
+  "github.com/btcsuite/btcd/txscript"
 )
 
 // SignatureEOSIO eosio transaction signature
@@ -56,4 +59,55 @@ func (s *WalletCoreServerRPC) SignatureEthereum(ctx context.Context, in *proto.S
   }
 
   return &proto.SignTxResp{Result: true, HexSignedTx: signedTx}, nil
+}
+
+// SignatureBitcoincore bitcoincore transaction signature
+func (s *WalletCoreServerRPC) SignatureBitcoincore(ctx context.Context, in *proto.SignatureBitcoincoreReq) (*proto.SignTxResp, error) {
+  ldb, err := db.NewLDB(db.BitcoinCoreLD)
+  if err != nil {
+    return nil, err
+  }
+  defer ldb.Close()
+
+  // query from address
+  priv, err := ldb.Get([]byte(in.From), nil)
+  if err != nil && strings.Contains(err.Error(), "leveldb: not found") {
+    return nil, fmt.Errorf("Address: %s not found %s", in.From, err)
+  }
+
+  // https://www.experts-exchange.com/questions/29108851/How-to-correctly-create-and-sign-a-Bitcoin-raw-transaction-using-Btcutil-library.html
+  tx, err := blockchain.DecodeBtcTxHex(in.RawTxHex)
+  if err != nil {
+    return nil, fmt.Errorf("Fail to decode raw tx %s", err)
+  }
+
+  wif, err := btcutil.DecodeWIF(string(priv[:]))
+  if err != nil {
+    return nil, fmt.Errorf("Fail to decode wif %s", err)
+  }
+  fromAddress, _ := btcutil.DecodeAddress(in.From, s.BTCNet)
+  subscript, _ := txscript.PayToAddrScript(fromAddress)
+  for i, txIn := range tx.MsgTx().TxIn {
+    sigScript, err := txscript.SignatureScript(tx.MsgTx(), i, subscript, txscript.SigHashAll, wif.PrivKey, true)
+    if err != nil {
+      return nil, fmt.Errorf("SignatureScript %s", err)
+    }
+    txIn.SignatureScript = sigScript
+  }
+
+  //Validate signature
+  flags := txscript.StandardVerifyFlags
+  vm, err := txscript.NewEngine(subscript, tx.MsgTx(), 0, flags, nil, nil, in.VinAmount)
+  if err != nil {
+    return nil, fmt.Errorf("Txscript.NewEngine %s", err)
+  }
+  if err := vm.Execute(); err != nil {
+    return nil, fmt.Errorf("Fail to sign tx %s", err)
+  }
+
+  // txToHex
+  buf := bytes.NewBuffer(make([]byte, 0, tx.MsgTx().SerializeSize()))
+  tx.MsgTx().Serialize(buf)
+  txHex := hex.EncodeToString(buf.Bytes())
+  return &proto.SignTxResp{Result: true, HexSignedTx: txHex}, nil
 }
