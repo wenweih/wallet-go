bb58e46802e36823856edb02ca950a502002d277
huangwenwei <mr.huangwenwei@gmail.com>
2019-03-17 08:18:52 +0800

    Remove wallet-coere bitcoinmode flag

--- cmd/wallet_core/main.go
+++ cmd/wallet_core/main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"flag"
 	"net"
 	"net/url"
 	"strings"
 	"google.golang.org/grpc"
 	pb "wallet-transition/pkg/pb"
 	"wallet-transition/pkg/rpc"
-	"wallet-transition/pkg/blockchain"
 	"wallet-transition/pkg/configure"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	// TODO: remove bitcoin mode flag, it should be determined by rpc client
-	var bitcoinmode string
-	flag.StringVar(&bitcoinmode, "bitcoinmode", "mainnet", "btc base chain mode: testnet, regtest or mainnet")
-	flag.Parse()
-	bitcoinNet, err := blockchain.BitcoinNet(bitcoinmode)
-	if err != nil {
-		configure.Sugar.Fatal(err.Error())
-	}
-
 	u, err := url.Parse(strings.Join([]string{"//", configure.Config.WalletCoreRPCURL}, ""))
 	if err != nil {
 		configure.Sugar.Info("Parse WalletRPCURL error: ", err.Error())
@@ -38,7 +27,7 @@ func main() {
 	}
 
 	rpcServer := grpc.NewServer()
-	pb.RegisterWalletCoreServer(rpcServer, &rpc.WalletCoreServerRPC{BTCNet: bitcoinNet})
+	pb.RegisterWalletCoreServer(rpcServer, &rpc.WalletCoreServerRPC{})
 	reflection.Register(rpcServer)
 	if err := rpcServer.Serve(lis); err != nil {
 		configure.Sugar.Info("failed to serve: ", err.Error())
--- pkg/rpc/types.go
+++ pkg/rpc/types.go
@@ -1,10 +1,4 @@
 package rpc
 
-import (
-  "github.com/btcsuite/btcd/chaincfg"
-)
-
 // WalletCoreServerRPC WalletCore rpc server
-type WalletCoreServerRPC struct {
-  BTCNet *chaincfg.Params
-}
+type WalletCoreServerRPC struct {}
