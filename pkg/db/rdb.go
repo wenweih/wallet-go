97c1d5287c0bb98c0d80592b30561991e0e9c97f
huangwenwei <mr.huangwenwei@gmail.com>
2019-03-28 00:14:42 +0800

    Add database TrackBlock for utxo

--- pkg/db/rdb.go
+++ pkg/db/rdb.go
@@ -44,6 +44,7 @@ func NewMySQL() (*GormDB, error) {
   }
   configure.Sugar.Info("database connecting...")
   db.AutoMigrate(&SubAddress{}, &SimpleBitcoinBlock{}, &UTXO{}, &transition.StateChangeLog{})
+  db.DB().SetMaxIdleConns(100)
   return &GormDB{db}, nil
 }
 
@@ -67,7 +68,7 @@ func (db *GormDB) GetBTCBestBlockOrCreate(block *btcjson.GetBlockVerboseResult, 
 // CreateBitcoinBlockWithUTXOs save block and utxo related with subAddress blockResultCh <-chan
 func (db *GormDB) CreateBitcoinBlockWithUTXOs(queryBlockResultCh <- chan common.QueryBlockResult) (<-chan common.CreateBlockResult) {
   createBlockCh := make(chan common.CreateBlockResult)
-  go func() {
+  go func(db *GormDB) {
     defer close(createBlockCh)
     var (
       rawBlock *btcjson.GetBlockVerboseResult
@@ -81,34 +82,48 @@ func (db *GormDB) CreateBitcoinBlockWithUTXOs(queryBlockResultCh <- chan common.
       rawBlock = b.Block.(*btcjson.GetBlockVerboseResult)
       chain = b.Chain
     }
-    var utxos []UTXO
+
+    var block SimpleBitcoinBlock
+    ts := db.Begin()
+    if err := ts.FirstOrCreate(&block, SimpleBitcoinBlock{
+      Hash: rawBlock.Hash,
+      Height: rawBlock.Height,
+      Chain: chain,
+    }).Error; err != nil {
+      ts.Rollback()
+      createBlockCh <- common.CreateBlockResult{Error: fmt.Errorf("create block error: %s", err)}
+      return
+    }
+
     for _, tx := range rawBlock.Tx {
       for _, vout := range tx.Vout {
         for _, address := range vout.ScriptPubKey.Addresses {
-          var addr SubAddress
+          var (
+            addr SubAddress
+            utxo UTXO
+          )
           if err := db.Where("address = ? AND asset = ?", address, chain).First(&addr).Error; err != nil && err.Error() == "record not found" {
             continue
           }else if err != nil {
             createBlockCh <- common.CreateBlockResult{Error: fmt.Errorf("Query sub address err: %s", err)}
             return
           }
-          utxo := UTXO{Txid: tx.Txid, Amount: vout.Value, Height: rawBlock.Height, VoutIndex: vout.N, SubAddress: addr}
-          utxos = append(utxos, utxo)
+          ts.FirstOrCreate(&utxo, UTXO{Txid: tx.Txid,
+            Amount: vout.Value,
+            Height: rawBlock.Height,
+            VoutIndex: vout.N,
+            SubAddressID: addr.ID})
         }
       }
     }
-    block := SimpleBitcoinBlock{
-      Hash: rawBlock.Hash,
-      Height: rawBlock.Height,
-      UTXOs: utxos,
-      Chain: chain,
-    }
-    if err := db.FirstOrCreate(&block).Error; err != nil {
-      createBlockCh <- common.CreateBlockResult{Error: fmt.Errorf("create block error: %s", err)}
+    if err := ts.Commit().Error; err != nil {
+      ts.Rollback()
+      createBlockCh <- common.CreateBlockResult{Error: fmt.Errorf("database transaction err: %s", err)}
       return
     }
-    createBlockCh <- common.CreateBlockResult{}
-  }()
+    configure.Sugar.Info("Saved block to database, height: ", block.Height, " hash: ", block.Hash)
+    createBlockCh <- common.CreateBlockResult{Block: block}
+  }(db)
   return createBlockCh
 }
 
@@ -150,7 +165,79 @@ func (db *GormDB) BlockInfo2DB(dbBlock SimpleBitcoinBlock, rawBlock *btcjson.Get
   return nil
 }
 
-// RollbackTrack rollback 6 blocks when add a new btc_block
+// TrackBlock rollback 6 blocks when save new block records
+func (db *GormDB) TrackBlock(bestBlockHeight int64, isTracking bool, queryBlockResultCh <- chan common.QueryBlockResult) (bool, int64) {
+  var (
+    rawBlock *btcjson.GetBlockVerboseResult
+    chain string
+    dbBlock SimpleBitcoinBlock
+    utxos []UTXO
+  )
+
+
+  for b := range queryBlockResultCh {
+    if b.Error != nil {
+      configure.Sugar.Fatal(b.Error.Error())
+    }
+    rawBlock = b.Block.(*btcjson.GetBlockVerboseResult)
+    chain = b.Chain
+  }
+
+  trackHeight := rawBlock.Height
+
+  if err := db.First(&dbBlock, "height = ? AND re_org = ?", rawBlock.Height, false).Related(&utxos).Error; err !=nil && err.Error() == "record not found" {
+    dbBlock.Hash = rawBlock.Hash
+    dbBlock.Height = rawBlock.Height
+    blockCh := make(chan common.QueryBlockResult)
+    go func (rawBlock *btcjson.GetBlockVerboseResult)  {
+      defer close(blockCh)
+      blockCh  <- common.QueryBlockResult{Block: rawBlock, Chain: chain}
+    }(rawBlock)
+    createBlockResul := <- db.CreateBitcoinBlockWithUTXOs(blockCh)
+    if createBlockResul.Error != nil{
+      configure.Sugar.Fatal(createBlockResul.Error.Error())
+    }
+
+    if createBlockResul.Error == nil{
+      bestBlock := createBlockResul.Block.(SimpleBitcoinBlock)
+      configure.Sugar.Info("create block successfully,", " height: ", bestBlock.Height, " hash: ", bestBlock.Hash)
+    }
+  }else if err != nil {
+    configure.Sugar.Fatal("query track block error:", err.Error())
+  }else {
+    if dbBlock.Hash != rawBlock.Hash {
+      ts := db.Begin()
+      // update utxos related with the dbBlock
+      ts.Model(&dbBlock).Update("re_org", true)
+      for _, utxo := range utxos {
+        ts.Model(&utxo).Update("re_org", true)
+      }
+      ts.Commit()
+
+      blockCh := make(chan common.QueryBlockResult)
+      blockCh  <- common.QueryBlockResult{Block: rawBlock, Chain: chain}
+      createBlockResul := <- db.CreateBitcoinBlockWithUTXOs(blockCh)
+      close(blockCh)
+      if createBlockResul.Error == nil{
+        bestBlock := createBlockResul.Block.(SimpleBitcoinBlock)
+        configure.Sugar.Info("create block successfully,", " height: ", bestBlock.Height, " hash: ", bestBlock.Hash)
+      }
+      configure.Sugar.Info("reorg:", dbBlock.Height, " ", dbBlock.Hash)
+    } else {
+      configure.Sugar.Info("tracking the same block, nothing happen ", dbBlock.Height)
+    }
+  }
+
+  if trackHeight < bestBlockHeight - 5 {
+    isTracking = false
+  }else {
+    isTracking = true
+    trackHeight --
+  }
+  return isTracking, trackHeight
+}
+
+// RollbackTrackBTC rollback 6 blocks when add a new btc_block
 func (db *GormDB) RollbackTrackBTC(bestHeight int64, backTracking bool, rawBlock *btcjson.GetBlockVerboseResult, chain string) (bool, int64) {
   trackHeight := rawBlock.Height
   var (
